9dc79e7f5eb62f854e13e1ddb9d541fec7c29a7b
covrazcyzhu <chenyang.zhu@myxxjs.com>
2017-06-16 20:26:51 +0800

    修改委托方

--- web/js/controller/setting/setting_client/setting_client_details_controller.js
+++ web/js/controller/setting/setting_client/setting_client_details_controller.js
@@ -1,6 +1,38 @@
 /**
  * Created by ASUS on 2017/6/8.
  */
-app.controller("setting_client_details_controller",["$scope","_basic","_config","$host",function ($scope,_basic,_config,$host){
+app.controller("setting_client_details_controller", ["$scope", "_basic", "$stateParams", "_config", "$host", function ($scope, _basic, $stateParams, _config, $host) {
+    $scope.userId = _basic.getSession(_basic.USER_ID);
+    // 设置文本框默认值
+    $scope.short_name = $stateParams.short_name;
+    $scope.full_name = $stateParams.entrust_name;
+    $scope.remark = "";
+    var val = $stateParams.id;
+    (function () {
+        console.log(val)
+    })();
+    $scope.entrust_modify = function () {
+        if ($scope.short_name != "" && $scope.full_name != "") {
+            _basic.put($host.api_url + "/user/" + $scope.userId + "/entrust/" + val, {
+                shortName: $scope.short_name,
+                entrustName: $scope.full_name,
+                remark: $scope.remark
+            }).then(function (data) {
+                if (data.success == true) {
+                    swal("修改成功", "", "success");
+                    $scope.short_name = "";
+                    $scope.full_name = "";
+                    $scope.remark = "";
+                }
+                else {
+                    swal(data.msg, "", "error");
+                }
+            })
+        }
+        else {
+            swal("请填写完整信息", "", "error");
+        }
+
+    }
 
 }]);

223935d1b9d72b8635aab4d765fe5cac5b67c5ce
covrazcyzhu <chenyang.zhu@myxxjs.com>
2017-07-04 17:19:53 +0800

    用户名及密码登录bug修改

--- web/js/controller/common_login_controller.js
+++ web/js/controller/common_login_controller.js
@@ -1,49 +1,49 @@
 /**
  * Created by jiangsen on 2017/4/11.
  */
-var common_login_controller=angular.module("common_login_controller",[]);
-common_login_controller.controller("common_login_controller", ['$rootScope','$scope','$location','$q',"_basic","$host","_config",
+var common_login_controller = angular.module("common_login_controller", []);
+common_login_controller.controller("common_login_controller", ['$rootScope', '$scope', '$location', '$q', "_basic", "$host", "_config",
 
-    function($rootScope,$scope,$location,$q,_basic,$host,_config){
-            $scope.username='';
-            $scope.password='';
-            $scope.login = function(){
+    function ($rootScope, $scope, $location, $q, _basic, $host, _config) {
+        $scope.username = '';
+        $scope.password = '';
+        $scope.login = function () {
 
-            if($scope.username==''||$scope.username==''){
+            if ($scope.username == '' || $scope.password == '') {
                 swal("账号或密码不能为空", "", "error");
             } else {
                 $(".shadeDowWrap").show();
-                _basic.post($host.api_url+"/userLogin", {
+                _basic.post($host.api_url + "/userLogin", {
                     "mobile": $scope.username,
                     "password": $scope.password
-                }).then(function(data){
+                }).then(function (data) {
                     $(".shadeDowWrap").hide();
-                    if(data.success==true){
-                        _basic.setSession(_basic.USER_AUTH_NAME,data.result.accessToken);
-                        _basic.setSession(_basic.USER_ID,data.result.userId);
-                        _basic.setSession(_basic.USER_STATUS,data.result.userStatus);
-                        _basic.setSession(_basic.USER_TYPE,data.result.type);
+                    if (data.success == true) {
+                        _basic.setSession(_basic.USER_AUTH_NAME, data.result.accessToken);
+                        _basic.setSession(_basic.USER_ID, data.result.userId);
+                        _basic.setSession(_basic.USER_STATUS, data.result.userStatus);
+                        _basic.setSession(_basic.USER_TYPE, data.result.type);
                         _basic.setHeader(_basic.USER_TYPE, data.result.type);
                         _basic.setHeader(_basic.COMMON_AUTH_NAME, data.result.accessToken);
                         // 判断user_type控制页面调到某个模块
                         /*if(data.result.type==_config.userTypes.storageUser.type){
-                            window.location.href="storage_home.html";
-                        }
-                        if(data.result.type==_config.userTypes.dispatch.type){
-                            window.location.href="dispatch_home.html";
-                        }
-                        if(data.result.type==_config.userTypes.international_trade.type){
-                            window.location.href="trade_home.html";
-                        }*/
-                        for(var i = 0; i<_config.userTypes.length;i++){
-                            if(_config.userTypes[i].type == data.result.type){
+                         window.location.href="storage_home.html";
+                         }
+                         if(data.result.type==_config.userTypes.dispatch.type){
+                         window.location.href="dispatch_home.html";
+                         }
+                         if(data.result.type==_config.userTypes.international_trade.type){
+                         window.location.href="trade_home.html";
+                         }*/
+                        for (var i = 0; i < _config.userTypes.length; i++) {
+                            if (_config.userTypes[i].type == data.result.type) {
                                 window.location.href = _config.userTypes[i].index;
                             }
                         }
-                    }else {
-                        swal(data.msg,"","error");
+                    } else {
+                        swal(data.msg, "", "error");
                     }
-                }).catch(function(error){
+                }).catch(function (error) {
                     swal("登录异常", "", "error");
                     console.log(error)
                 });

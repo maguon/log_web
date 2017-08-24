4ec704dead6a6164aec42b9713bac042f7267be8
joesBel <sen.jiang@myxxjs.com>
2017-08-24 17:20:12 +0800

    指令列表  需求列表

--- web/js/controller/dispatch/look_instruction_list_details_controller.js
+++ web/js/controller/dispatch/look_instruction_list_details_controller.js
@@ -6,6 +6,7 @@ app.controller("look_instruction_list_details_controller", ["$scope", "$host", "
     $scope.return=function () {
         $state.go($stateParams.from,{reload:true})
     };
+    $scope.LoadTaskList=false;
     function p() {
         var p=new Promise(function (resolve,reject) {
             _basic.get($host.api_url+"/dpRouteTask?dpRouteTaskId="+$stateParams.id).then(function (data) {
@@ -24,5 +25,27 @@ app.controller("look_instruction_list_details_controller", ["$scope", "$host", "
                 $scope.this_LoadTask=data.result;
             }
         })
-    })
+    });
+    $scope.open_LoadTaskList=function (id,index) {
+        $(".this_LoadTaskList").hide();
+        if($(".this_LoadTaskList"+index).attr("flag")=='false'){
+            _basic.get($host.api_url+"/dpRouteLoadTask/"+id+"/dpRouteLoadTaskDetail").then(function (data) {
+                $(".this_LoadTaskList").attr("flag","false");
+                if(data.success==true&&data.result.length>0){
+                    $scope.this_LoadTaskList=data.result;
+
+                    $(".this_LoadTaskList"+index).show();
+                    $(".this_LoadTaskList"+index).attr("flag",'true');
+                }else {
+                    $(".this_LoadTaskList"+index).attr("flag",'false');
+                    $(".this_LoadTaskList"+index).hide();
+                }
+            })
+        }else {
+            $(".this_LoadTaskList"+index).attr("flag",'false');
+            $(".this_LoadTaskList"+index).hide();
+        }
+
+       ;
+    }
 }]);

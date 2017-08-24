3928fb0ae81922e353bee29eefc4c5a96f91c236
joesBel <sen.jiang@myxxjs.com>
2017-08-24 13:58:09 +0800

    指令列表  需求列表

--- web/js/controller/dispatch/instruction_need_controller.js
+++ web/js/controller/dispatch/instruction_need_controller.js
@@ -4,54 +4,91 @@
 app.controller("instruction_need_controller", ["$scope", "$host","_config","_basic", function ($scope, $host,_config, _basic) {
     // 指令任务状态
     $scope.taskStatusList=_config.taskStatus;
-    // _basic.get($host.api_url+"/city").then(function (data) {
-    //     if(data.success==true){
-    //         $scope.cityList=data.result;
-    //     }
-    // });
-    // // 获取装车地点
-    // $scope.getAddres=function (id) {
-    //     _basic.get($host.api_url+"/baseAddr?cityId="+id).then(function (data) {
-    //         if(data.success==true){
-    //             $scope.baseAddrList=data.result;
-    //         }
-    //     });
-    // };
-    // // 获取经销商
-    // $scope.getRecive=function (id) {
-    //     _basic.get($host.api_url+"/receive?cityId="+id).then(function (data) {
-    //         if(data.success==true){
-    //             $scope.receiveList=data.result;
-    //         }
-    //     });
-    // };
+    _basic.get($host.api_url+"/city").then(function (data) {
+        if(data.success==true){
+            $scope.cityList=data.result;
+        }
+    });
+    // 获取装车地点
+    $scope.getAddres=function (id) {
+        _basic.get($host.api_url+"/baseAddr?cityId="+id).then(function (data) {
+            if(data.success==true){
+                $scope.baseAddrList=data.result;
+            }
+        });
+    };
+    // 获取经销商
+    $scope.getRecive=function (id) {
+        _basic.get($host.api_url+"/receive?cityId="+id).then(function (data) {
+            if(data.success==true){
+                $scope.receiveList=data.result;
+            }
+        });
+    };
     $scope.add_need=function () {
         $('.modal').modal();
         $('#newNeed').modal('open');
+        var data = new Date();
+        $scope.time = moment(data).format('YYYY-MM-DD');
+        $scope.user=_basic.getSession(_basic.USER_NAME)
+        $scope.submitted=false;
+        $scope.add_start_city.id="",
+        $scope.add_start_city.city_name='',
+        $scope.add_dispatch_car_position='',
+        $scope.add_end_city.id='',
+        $scope.add_end_city.city_name='',
+        $scope.add_dealer='',
+        $scope.add_car_num='',
+        $scope.add_instruct_Time=''
     }
     $scope.search_all=function () {
 
         var obj={
-            dpRouteTaskId:"",
-            vin:$scope.vin,
-            taskPlanDateStart:$scope.instruct_startTime,
-            taskPlanDateEnd:$scope.instruct_endTime,
-            truckId:"",
-            truckNum:$scope.truck_num,
-            driveName:$scope.driver,
+            demandDateStart:$scope.instruct_need_startTime,
+            demandDateEnd:$scope.instruct_need_endTime,
+            dateIdStart:$scope.instruct_start_time,
+            dateIdEnd:$scope.instruct_endTime,
+            realName:$scope.instruct_need_man,
+            preCountStart:$scope.car_num_start,
+            preCountEnd:$scope.car_num_end,
             routeStartId:$scope.start_city,
             baseAddrId:$scope.dispatch_car_position,
-            routeEndId:$scope.end_city,
+            routeEndId:$scope.arrive_city,
             receiveId:$scope.dealer,
-            taskStatus:$scope.task_status
+            // demandStatus:$scope.task_status
         };
-        _basic.get($host.api_url+"/dpRouteTask?"+_basic.objToUrl(obj)).then(function (data) {
+        _basic.get($host.api_url+"/dpDemand?"+_basic.objToUrl(obj)).then(function (data) {
             if(data.success==true&&data.result.length>0){
-                $scope.instruction_list=data.result;
+                $scope.instruction_neee_list=data.result;
             }else {
                 $scope.instruction_list=[];
             }
         })
     };
     $scope.search_all();
+    
+    // 新增需求
+    $scope.add_need_instruct=function (inValid) {
+        $scope.submitted=true;
+        if(inValid){
+           var obj={
+               "routeStartId": $scope.add_start_city.id,
+               "routeStart": $scope.add_start_city.city_name,
+               "baseAddrId": $scope.add_dispatch_car_position,
+               "routeEndId": $scope.add_end_city.id,
+               "routeEnd":  $scope.add_end_city.city_name,
+               "receiveId": $scope.add_dealer,
+               "preCount":$scope.add_car_num,
+               "dateId": $scope.add_instruct_Time
+           };
+            _basic.post($host.api_url+"/user/"+_basic.getSession(_basic.USER_ID)+"/dpDemand",obj).then(function (data) {
+                if(data.success==true){
+                    $scope.submitted=false;
+                    swal("新增成功","","success");
+                    $('#newNeed').modal('close');
+                    $scope.search_all();
+                }
+            })
+        }
+    }
 }]);

0cdd15a8d0bf7ed0381e5c9c8bd21e885045fc53
covrazcyzhu <chenyang.zhu@myxxjs.com>
2017-10-19 14:41:49 +0800

    修改调度需求下需求详情显示问题

--- web/js/controller/dispatch/look_instruction_need_details_controller.js
+++ web/js/controller/dispatch/look_instruction_need_details_controller.js
@@ -3,53 +3,72 @@
  */
 
 
-app.controller("look_instruction_need_details_controller", ["$scope", "$host", "_basic","$state","$stateParams", function ($scope, $host, _basic,$state,$stateParams) {
-    $scope.return=function () {
-            $state.go($stateParams.from,{reload:true})
+app.controller("look_instruction_need_details_controller", ["$scope", "$host", "_basic", "$state", "$stateParams", function ($scope, $host, _basic, $state, $stateParams) {
+    $scope.return = function () {
+        $state.go($stateParams.from, {reload: true})
     };
-    $scope.LoadTaskList=false;
-    function p(){
-        var p=new Promise(function (resolve,reject) {
-            _basic.get($host.api_url+"/dpDemand?dpDemandId="+$stateParams.id).then(function (data) {
-                if(data.success==true&&data.result.length>0){
-                    $scope.this_instruction=data.result[0];
-                    if($scope.this_instruction.date_id){
-                        $scope.data_id=moment($scope.this_instruction.date_id.toString()).format("YYYY-MM-DD");
-                    }else {
-                        $scope.data_id="";
-                    }
-                    resolve();
-                }
-            })
-        });
-        return p;
-    }
+    $scope.LoadTaskList = false;
 
-    p().then(function (){
-        _basic.get($host.api_url+"/dpRouteLoadTask?dpRouteTaskId="+$scope._id).then(function (data) {
-            if(data.success==true){
-                $scope.this_LoadTask=data.result;
+    // 获取需求详情信息
+    $scope.getDispatchInfo = function () {
+        _basic.get($host.api_url + "/dpDemand?dpDemandId=" + $stateParams.id).then(function (data) {
+            if (data.success === true && data.result.length > 0) {
+                $scope.this_instruction = data.result[0];
+                if ($scope.this_instruction.date_id) {
+                    $scope.data_id = moment($scope.this_instruction.date_id.toString()).format("YYYY-MM-DD");
+                }
+                else {
+                    $scope.data_id = "";
+                }
             }
-        })
-    });
-    $scope.open_LoadTaskList=function (id,index) {
+        });
+
+        _basic.get($host.api_url + "/dpRouteLoadTask?dpDemandId=" + $stateParams.id).then(function (data) {
+            if (data.success === true) {
+                console.log("data",data);
+                $scope.disPatchInfoList = data.result;
+            }
+            else {
+                swal(data.msg, "", "error");
+            }
+        });
+    };
+
+
+    // p().then(function () {
+    //     _basic.get($host.api_url + "/dpRouteLoadTask?dpRouteTaskId=" + $scope._id).then(function (data) {
+    //         if (data.success == true) {
+    //             $scope.this_LoadTask = data.result;
+    //         }
+    //     })
+    // });
+
+
+    $scope.open_LoadTaskList = function (id, index) {
         $(".this_LoadTaskList").hide();
-        if($(".this_LoadTaskList"+index).attr("flag")=='false'){
-            _basic.get($host.api_url+"/dpRouteLoadTask/"+id+"/dpRouteLoadTaskDetail").then(function (data) {
-                $(".this_LoadTaskList").attr("flag","false");
-                if(data.success==true&&data.result.length>0){
-                    $scope.this_LoadTaskList=data.result;
-                    $(".this_LoadTaskList"+index).show();
-                    $(".this_LoadTaskList"+index).attr("flag",'true');
-                }else {
-                    $(".this_LoadTaskList"+index).attr("flag",'false');
-                    $(".this_LoadTaskList"+index).hide();
+        if ($(".this_LoadTaskList" + index).attr("flag") == 'false') {
+            _basic.get($host.api_url + "/dpRouteLoadTask/" + id + "/dpRouteLoadTaskDetail").then(function (data) {
+                $(".this_LoadTaskList").attr("flag", "false");
+                if (data.success == true && data.result.length > 0) {
+                    console.log("vinData",data);
+                    $scope.this_LoadTaskList = data.result;
+                    $(".this_LoadTaskList" + index).show();
+                    $(".this_LoadTaskList" + index).attr("flag", 'true');
+                }
+                else {
+                    $(".this_LoadTaskList" + index).attr("flag", 'false');
+                    $(".this_LoadTaskList" + index).hide();
                 }
             })
-        }else {
-            $(".this_LoadTaskList"+index).attr("flag",'false');
-            $(".this_LoadTaskList"+index).hide();
+        }
+        else {
+            $(".this_LoadTaskList" + index).attr("flag", 'false');
+            $(".this_LoadTaskList" + index).hide();
+        }
+    };
 
-        };
-    }
+    $scope.queryData = function () {
+        $scope.getDispatchInfo();
+    };
+    $scope.queryData();
 }]);

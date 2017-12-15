95d0563db6877cee476859d37487f141f2a58bb3
covrazcyzhu <chenyang.zhu@myxxjs.com>
2017-12-15 17:20:07 +0800

    add Password recovery function

--- web/js/controller/retrieve_password_controller.js
+++ web/js/controller/retrieve_password_controller.js
@@ -1,33 +1,76 @@
 /**
  * Created by ASUS on 2017/9/25.
  */
-Login_model.controller("retrieve_password_controller", ["$state",'$rootScope', '$scope', '$location', '$q', "_basic", "$host", "_config",
-
-    function ($state,$rootScope, $scope, $location, $q, _basic, $host, _config) {
+Login_model.controller("retrieve_password_controller", ["$state", "$scope", "$q", "$interval", "_basic", "$host", function ($state, $scope, $q, $interval, _basic, $host) {
     $scope.mobileRegx = "^1(3[0-9]|4[57]|5[0-35-9]|7[01678]|8[0-9])\\d{8}$";
-            $scope.return=function () {
-                $state.go("common_login")
-                $scope.submitted=false;
-                $scope.phoneNum="";
-                $scope.newPsw="";
-                $scope.ConfirmNewPsw="";
-                $scope.verificationCode="";
-            };
-        $scope.retrieve_password=function (valid) {
-            $scope.submitted=true;
-            if(valid&&$scope.newPsw==$scope.confirmNewPsw){
-                var obj={
-                    "mobile":$scope.phoneNum,
-                    "newPassword":$scope.newPsw,
-                    "confirmNewPassword":$scope.confirmNewPsw
+
+    // 返回上一级
+    $scope.return = function () {
+        $state.go("common_login");
+        $scope.submitted = false;
+        $scope.phoneNum = "";
+        $scope.newPsw = "";
+        $scope.ConfirmNewPsw = "";
+        $scope.verificationCode = "";
+    };
+
+    // 发送手机验证码
+    $scope.sendVerificationCode = function () {
+        if (/^1(3[0-9]|4[57]|5[0-35-9]|7[01678]|8[0-9])\d{8}$/.test($scope.phoneNum)) {
+            $scope.timeCount = 60;
+            _basic.post($host.api_url + "/phone/" + $scope.phoneNum + "/passwordSms", {}).then(function (data) {
+                if (data.success === true) {
+                    swal("验证码已发送", "", "success");
+                    $("#sendVerification").attr("disabled", true);
+                    var countDown = $interval(function () {
+                        if ($scope.timeCount > 0) {
+                            $scope.timeCount--;
+                            $scope.btnTip = "(" + $scope.timeCount + ")";
+                        }
+                        else{
+                            $scope.btnTip = "";
+                            $interval.cancel(countDown);
+                            $("#sendVerification").attr("disabled", false);
+                        }
+                    }, 1000);
+
+                }
+                else {
+                    swal(data.msg, "", "error");
+                }
+            });
+        }
+        else {
+            swal("请填写有效手机号！", "", "error");
+        }
+    };
+
+
+    // 点击提交按钮
+    $scope.retrieve_password = function (valid) {
+        $scope.submitted = true;
+        if ($scope.newPsw === $scope.confirmNewPsw) {
+            if (valid) {
+                var obj = {
+                    captcha:$scope.verificationCode,
+                    password:$scope.confirmNewPsw
                 };
-                _basic.put($host.api_url +"/backPassword", obj).then(function (data) {
+                _basic.put($host.api_url + "/phone/" + $scope.phoneNum + "/password", obj).then(function (data) {
                     if (data.success == true) {
                         swal("密码重置成功", "", "success");
-                    } else {
+                    }
+                    else {
                         swal(data.msg, "", "error");
                     }
                 })
             }
-        };
-    }]);
+            else {
+                swal("信息有误，请重新填写", "", "error");
+            }
+        }
+        else {
+            swal("两次密码输入不一致！", "", "error");
+        }
+
+    };
+}]);

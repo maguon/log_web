4a83b991e7708fac14c264418fe537849b502dc4
covrazcyzhu <chenyang.zhu@myxxjs.com>
2018-01-30 15:17:52 +0800

    modify damage_declaration table and date picker

--- web/js/controller/damage/damage_declaration_controller.js
+++ web/js/controller/damage/damage_declaration_controller.js
@@ -1,39 +1,62 @@
 app.controller("damage_declaration_controller", ["$scope", "$host", "_basic", function ($scope, $host, _basic) {
 
-    // monthPicker日历控件
-    $('#report_time_start,#report_time_end').MonthPicker({
-        Button: false,
-        MonthFormat: 'yy-mm'
-    });
-    // moment(pastMissionData.result[i].date_id.toString()).format("YYYY-MM-DD");
-    $scope.reportTimeStart = "";
-    $scope.reportTimeEnd = "";
+    $scope.start = 0;
+    $scope.size = 20;
     $scope.processingStatus = "";
     $scope.vinCode = "";
 
     // 获取质损列表
     $scope.getDamageInfoList = function () {
-        var reportTimeStart = $("#report_time_start").val();
-        var reportTimeEnd = $("#report_time_end").val();
         _basic.get($host.api_url + "/damage?" + _basic.objToUrl({
-            createdOnStart: reportTimeStart,
-            createdOnEnd: reportTimeEnd,
+            createdOnStart: $scope.reportTimeStart,
+            createdOnEnd: $scope.reportTimeEnd,
             damageStatus: $scope.processingStatus,
-            vin:$scope.vinCode
+            vin:$scope.vinCode,
+            start:$scope.start.toString(),
+            size:$scope.size
         })).then(function (damageData) {
             if (damageData.success === true) {
+                if ($scope.start > 0) {
+                    $("#pre").show();
+                }
+                else {
+                    $("#pre").hide();
+                }
+                if (damageData.result.length < $scope.size) {
+                    $("#next").hide();
+                }
+                else {
+                    $("#next").show();
+                }
                 // console.log("damageData",damageData);
                 $scope.damageList = damageData.result;
             }
             else {
-                swal(data.msg, "", "error");
+                swal(damageData.msg, "", "error");
             }
         });
     };
 
+    // 点击按钮进行查询
+    $scope.searchDamageInfoList = function () {
+        $scope.start = 0;
+        $scope.getDamageInfoList();
+    };
+
+    // 分页
+    $scope.previous_page = function () {
+        $scope.start = $scope.start - $scope.size;
+        $scope.getDamageInfoList();
+    };
+
+    $scope.next_page = function () {
+        $scope.start = $scope.start + $scope.size;
+        $scope.getDamageInfoList();
+    };
+
     // 获取数据
     $scope.queryData = function () {
-        $scope.getDamageInfoList()
+        $scope.searchDamageInfoList()
     };
     $scope.queryData();
 }]);

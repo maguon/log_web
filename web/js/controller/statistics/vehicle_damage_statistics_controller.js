880d47d7be903f693b9e0ee72698689167a28e14
covrazcyzhu <chenyang.zhu@myxxjs.com>
2018-02-28 10:12:53 +0800

    modify vehicle_damage_statistics damage level

--- web/js/controller/statistics/vehicle_damage_statistics_controller.js
+++ web/js/controller/statistics/vehicle_damage_statistics_controller.js
@@ -8,34 +8,67 @@ app.controller("vehicle_damage_statistics_controller", ["$scope", "$host", "_bas
     });
     var month;
     var week;
+
     // 商品车质损按月统计
     var carDamageCountMonth = [
         {
-            name: '一般质损',
+            name: 'A级',
             data: [],
             color: '#FF7E7E'
         },
         {
-            name: '严重质损',
+            name: 'B级',
             data: [],
             color: '#26C6DA'
+        },
+        {
+            name: 'C级',
+            data: [],
+            color: '#F5AA2C'
+        },
+        {
+            name: 'D级',
+            data: [],
+            color: '#BF19E1'
+        },
+        {
+            name: 'F级',
+            data: [],
+            color: '#40da49'
         }
     ];
+
     // 商品车质损按周统计
     var carDamageCountWeek = [
         {
-            name: '一般质损',
+            name: 'A级',
             data: [],
             color: '#FF7E7E'
         },
         {
-            name: '严重质损',
+            name: 'B级',
             data: [],
             color: '#26C6DA'
+        },
+        {
+            name: 'C级',
+            data: [],
+            color: '#F5AA2C'
+        },
+        {
+            name: 'D级',
+            data: [],
+            color: '#BF19E1'
+        },
+        {
+            name: 'F级',
+            data: [],
+            color: '#40da49'
         }
     ];
+
     // 显示商品车质损金额按月统计折线图
-    function showVehicleDamageHistogramMonth () {
+    function showVehicleDamageHistogramMonth() {
         $("#carDamageStatisticsMonth").highcharts({
             title: {
                 text: ''
@@ -44,7 +77,7 @@ app.controller("vehicle_damage_statistics_controller", ["$scope", "$host", "_bas
                 text: ''
             },
             xAxis: {
-                categories:month,
+                categories: month,
                 crosshair: true
             },
             yAxis: {
@@ -72,9 +105,10 @@ app.controller("vehicle_damage_statistics_controller", ["$scope", "$host", "_bas
             },
             series: carDamageCountMonth
         });
-    };
+    }
+
     // 显示商品车质损金额按周统计折线图
-    function showVehicleDamageHistogramWeek () {
+    function showVehicleDamageHistogramWeek() {
         $("#carDamageStatisticsWeek").highcharts({
             chart: {
                 type: 'line'
@@ -86,7 +120,7 @@ app.controller("vehicle_damage_statistics_controller", ["$scope", "$host", "_bas
                 text: ''
             },
             xAxis: {
-                categories:week,
+                categories: week,
                 crosshair: true
             },
             yAxis: {
@@ -114,87 +148,125 @@ app.controller("vehicle_damage_statistics_controller", ["$scope", "$host", "_bas
             },
             series: carDamageCountWeek
         });
-    };
+    }
+
     // 按月
-    function vehicleDamageMonth(start,end){
+    function vehicleDamageMonth(start, end) {
         var obj = {
-            monthStart:start,
+            monthStart: start,
             monthEnd: end
-        }
-        _basic.get($host.api_url + "/damageTypeMonthStat?damageStatus=3&"+_basic.objToUrl(obj)).then(function (data) {
-            if (data.success === true){
+        };
+        _basic.get($host.api_url + "/damageTypeMonthStat?damageStatus=3&" + _basic.objToUrl(obj)).then(function (data) {
+            if (data.success === true) {
+                // console.log("data",data);
                 // data.result.reverse();
                 // X轴月份
                 month = [];
                 // 初始化金额数
                 carDamageCountMonth[0].data = [];
                 carDamageCountMonth[1].data = [];
+                carDamageCountMonth[2].data = [];
+                carDamageCountMonth[3].data = [];
+                carDamageCountMonth[4].data = [];
                 // 赋予柱状图金额数组
                 for (var i = 0; i < data.result.length; i++) {
-                    if(month.indexOf(data.result[i].y_month) === -1){
+                    if (month.indexOf(data.result[i].y_month) === -1) {
                         month.push(data.result[i].y_month);
                     }
-                    if(data.result[i].id==1){
+                    if (data.result[i].id === 1) {
                         carDamageCountMonth[0].data.push(data.result[i].damage_count);
                     }
-                    else if(data.result[i].id==2){
+                    if (data.result[i].id === 2) {
                         carDamageCountMonth[1].data.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id === 3) {
+                        carDamageCountMonth[2].data.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id === 4) {
+                        carDamageCountMonth[3].data.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id === 6) {
+                        carDamageCountMonth[4].data.push(data.result[i].damage_count);
                     }
                 }
                 showVehicleDamageHistogramMonth();
-            } else{
+            }
+            else {
                 swal(data.msg, "", "error");
             }
         });
     }
+
     //通过接口获取时间
     $scope.selectDamageDate = function () {
         var monthStart = $("#chooseRepairStart").val();
         var monthEnd = $("#chooseRepairEnd").val();
-        if(monthStart==''||monthStart == null||monthEnd==''||monthEnd == null){
-            monthStart= $scope.startInitial;
-            monthEnd=$scope.endInitial;
+        if (monthStart == '' || monthStart == null || monthEnd == '' || monthEnd == null) {
+            monthStart = $scope.startInitial;
+            monthEnd = $scope.endInitial;
             swal('请输入完整的时间信息', "", "error");
         }
         vehicleDamageMonth(monthStart, monthEnd);
     };
+
     //按周
     function vehicleDamageWeek() {
         _basic.get($host.api_url + "/damageTypeWeekStat?damageStatus=3").then(function (data) {
             if (data.success == true) {
+                // console.log("data",data);
                 data.result.reverse();
                 // X轴月份
                 week = [];
-                var normalWeek=[];
-                var seriousWeek=[];
+                var levelACount = [];
+                var levelBCount = [];
+                var levelCCount = [];
+                var levelDCount = [];
+                var levelFCount = [];
                 var weekAll = [];
                 // 初始化金额数
                 carDamageCountWeek[0].data = [];
                 carDamageCountWeek[1].data = [];
+                carDamageCountWeek[2].data = [];
+                carDamageCountWeek[3].data = [];
+                carDamageCountWeek[4].data = [];
                 // 赋予柱状图金额数组
                 for (var i = 0; i < data.result.length; i++) {
-                    if(weekAll.indexOf(data.result[i].y_week) === -1){
+                    if (weekAll.indexOf(data.result[i].y_week) === -1) {
                         weekAll.push(data.result[i].y_week);
                     }
-                    if(data.result[i].id==1){
-                        normalWeek.push(data.result[i].damage_count);
+                    if (data.result[i].id == 1) {
+                        levelACount.push(data.result[i].damage_count);
                     }
-                    else if(data.result[i].id==2){
-                        seriousWeek.push(data.result[i].damage_count);
+                    if (data.result[i].id == 2) {
+                        levelBCount.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id == 3) {
+                        levelCCount.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id == 4) {
+                        levelDCount.push(data.result[i].damage_count);
+                    }
+                    if (data.result[i].id == 6) {
+                        levelFCount.push(data.result[i].damage_count);
                     }
                 }
-                week=weekAll.slice(-10);
-                carDamageCountWeek[0].data=normalWeek.slice(-10);
-                carDamageCountWeek[1].data=seriousWeek.slice(-10);
+                week = weekAll.slice(-10);
+                carDamageCountWeek[0].data = levelACount.slice(-10);
+                carDamageCountWeek[1].data = levelBCount.slice(-10);
+                carDamageCountWeek[2].data = levelCCount.slice(-10);
+                carDamageCountWeek[3].data = levelDCount.slice(-10);
+                carDamageCountWeek[4].data = levelFCount.slice(-10);
                 showVehicleDamageHistogramWeek();
-            } else {
+            }
+            else {
                 swal(data.msg, "", "error");
             }
         });
     }
+
     // 获取数据
     $scope.queryData = function () {
-        vehicleDamageMonth($scope.startInitial,$scope.endInitial);
+        vehicleDamageMonth($scope.startInitial, $scope.endInitial);
         vehicleDamageWeek();
     };
     $scope.queryData();

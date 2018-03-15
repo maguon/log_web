fb2e89c3c2c4b443bfa2e4079ef4de97e99faaaa
covrazcyzhu <chenyang.zhu@myxxjs.com>
2018-03-15 17:04:43 +0800

    modify vehicle_index page

--- web/js/controller/storage/vehicle_index_controller.js
+++ web/js/controller/storage/vehicle_index_controller.js
@@ -33,18 +33,20 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 所属类型信息
         _basic.get($host.api_url + "/operateTypeCount?truckType=1").then(function (data) {
             if (data.success === true) {
-                for (var i = 0; i < data.result.length; i++) {
-                    if (data.result[i].operate_type === 1) {
-                        $scope.type_1_count = data.result[i].truck_count
-                    }
-                    if (data.result[i].operate_type === 2) {
-                        $scope.type_2_count = data.result[i].truck_count
-                    }
-                    if (data.result[i].operate_type === 3) {
-                        $scope.type_3_count = data.result[i].truck_count
-                    }
-                    if (data.result[i].operate_type === 4) {
-                        $scope.type_4_count = data.result[i].truck_count
+                if(data.result.length !== 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        if (data.result[i].operate_type === 1) {
+                            $scope.type_1_count = data.result[i].truck_count
+                        }
+                        if (data.result[i].operate_type === 2) {
+                            $scope.type_2_count = data.result[i].truck_count
+                        }
+                        if (data.result[i].operate_type === 3) {
+                            $scope.type_3_count = data.result[i].truck_count
+                        }
+                        if (data.result[i].operate_type === 4) {
+                            $scope.type_4_count = data.result[i].truck_count
+                        }
                     }
                 }
                 // console.log("truckData", data);
@@ -57,7 +59,9 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 自营挂车数量
         _basic.get($host.api_url + "/truckCount?truckType=2&operateType=1").then(function (data) {
             if (data.success === true) {
-                $scope.trailer = data.result[0].truck_count;
+                if(data.result.length !== 0){
+                    $scope.trailer = data.result[0].truck_count;
+                }
                 // console.log("data", data);
             }
             else {
@@ -68,21 +72,23 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 在职司机数量
         _basic.get($host.api_url + "/driveOperateTypeCount?driveStatus=1").then(function (data) {
             if (data.success === true) {
-                $scope.driverTotal = 0;
-                for (var i = 0; i < data.result.length; i++) {
-                    if (data.result[i].operate_type === 1) {
-                        $scope.driver_type_1_count = data.result[i].drive_count
+                if(data.result.length !== 0){
+                    $scope.driverTotal = 0;
+                    for (var i = 0; i < data.result.length; i++) {
+                        if (data.result[i].operate_type === 1) {
+                            $scope.driver_type_1_count = data.result[i].drive_count
+                        }
+                        if (data.result[i].operate_type === 2) {
+                            $scope.driver_type_2_count = data.result[i].drive_count
+                        }
+                        if (data.result[i].operate_type === 3) {
+                            $scope.driver_type_3_count = data.result[i].drive_count
+                        }
+                        if (data.result[i].operate_type === 4) {
+                            $scope.driver_type_4_count = data.result[i].drive_count
+                        }
+                        $scope.driverTotal = $scope.driverTotal + data.result[i].drive_count;
                     }
-                    if (data.result[i].operate_type === 2) {
-                        $scope.driver_type_2_count = data.result[i].drive_count
-                    }
-                    if (data.result[i].operate_type === 3) {
-                        $scope.driver_type_3_count = data.result[i].drive_count
-                    }
-                    if (data.result[i].operate_type === 4) {
-                        $scope.driver_type_4_count = data.result[i].drive_count
-                    }
-                    $scope.driverTotal = $scope.driverTotal + data.result[i].drive_count;
                 }
             }
             else {
@@ -96,7 +102,9 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 维修车辆数量
         _basic.get($host.api_url + "/truckRepairRelCount?repairStatus=0").then(function (data) {
             if (data.success === true) {
-                $scope.truckRepairCount = data.result[0].repair_count;
+                if(data.result.length !== 0){
+                    $scope.truckRepairCount = data.result[0].repair_count;
+                }
             }
             else {
                 swal(data.msg, "", "error");
@@ -106,7 +114,9 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 待检车辆
         _basic.get($host.api_url + "/truckFirst?drivingDateEnd=" + searchEnd).then(function (data) {
             if (data.success === true) {
-                $scope.quarantineTruck = data.result.length;
+                if(data.result.length !== 0){
+                    $scope.quarantineTruck = data.result.length;
+                }
                 // console.log("dateData", data)
             }
             else {
@@ -117,7 +127,9 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         // 待检司机
         _basic.get($host.api_url + "/drive?licenseDateEnd=" + searchEnd).then(function (data) {
             if (data.success === true) {
-                $scope.quarantineDriver = data.result.length;
+                if(data.result.length !== 0){
+                    $scope.quarantineDriver = data.result.length;
+                }
                 // console.log("driver", data)
             }
             else {
@@ -132,15 +144,17 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         _basic.get($host.api_url + "/truckAccidentNotCheckCount").then(function (data) {
             if (data.success === true) {
                 // console.log("data", data);
-                for (var i = 0; i < data.result.length; i++) {
-                    if(data.result[i].accident_status === 1){
-                        $scope.accidentTreatedCount = data.result[i].truck_accident_count;
+                if(data.result.length !== 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        if(data.result[i].accident_status === 1){
+                            $scope.accidentTreatedCount = data.result[i].truck_accident_count;
+                        }
+                        if(data.result[i].accident_status === 2){
+                            $scope.accidentTreatmentCount = data.result[i].truck_accident_count;
+                        }
                     }
-                    if(data.result[i].accident_status === 2){
-                        $scope.accidentTreatmentCount = data.result[i].truck_accident_count;
-                    }
+                    $scope.accidentTotal = $scope.accidentTreatedCount + $scope.accidentTreatmentCount;
                 }
-                $scope.accidentTotal = $scope.accidentTreatedCount + $scope.accidentTreatmentCount;
             }
             else {
                 swal(data.msg, "", "error");
@@ -151,9 +165,11 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         _basic.get($host.api_url + "/truckAccidentTotalCost?accidentStatus=3").then(function (data) {
             if (data.success === true) {
                 // console.log("data", data);
-                $scope.accidentBearCompany = data.result[0].company_cost.toFixed(2);
-                $scope.accidentBearPersonal = data.result[0].under_cost.toFixed(2);
-                $scope.accidentBearTotal = (data.result[0].company_cost + data.result[0].under_cost).toFixed(2);
+                if(data.result.length !== 0){
+                    $scope.accidentBearCompany = data.result[0].company_cost.toFixed(2);
+                    $scope.accidentBearPersonal = data.result[0].under_cost.toFixed(2);
+                    $scope.accidentBearTotal = (data.result[0].company_cost + data.result[0].under_cost).toFixed(2);
+                }
             }
             else {
                 swal(data.msg, "", "error");
@@ -164,8 +180,10 @@ app.controller("vehicle_index_controller", ['$scope', "$host", "_basic", functio
         _basic.get($host.api_url + "/truckAccidentInsurePlanTotal?insureStatus=1").then(function (data) {
             if (data.success === true) {
                 // console.log("data", data);
-                $scope.insurePlanCount = data.result[0].insure_plan_count;
-                $scope.insurePlanMoney = data.result[0].insure_plan.toFixed(2)
+                if(data.result.length !== 0){
+                    $scope.insurePlanCount = data.result[0].insure_plan_count;
+                    $scope.insurePlanMoney = data.result[0].insure_plan.toFixed(2)
+                }
             }
             else {
                 swal(data.msg, "", "error");

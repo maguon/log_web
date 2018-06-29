6567f40663f257e40814488fad951e896e29211c
star920626 <jing.zhang@myxxjs.com>
2018-06-29 11:23:35 +0800

    经销商加编号 添加结算主控

---
+++ web/js/controller/settlement/settlement_index_controller.js
@@ -0,0 +1,3 @@
+app.controller("settlement_index_controller", ["$scope", "$host", "_basic", function ($scope, $host, _basic) {
+
+}])
---
+++ web/js/router/settlement_home_router.js
@@ -0,0 +1,129 @@
+
+app.config(['$stateProvider', "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/settlement_index");
+    $stateProvider
+        .state("settlement_index",{
+            url:"/settlement_index",
+            templateUrl: "js/view/settlement/settlement_index.html",
+            controller:'settlement_index_controller'
+        })
+        // 用户信息
+        .state("user_info",{
+            url:"/user_info",
+            templateUrl: "js/view/user/user_info.html",
+            controller:'user_info_controller'
+        })
+
+        // 公共數據
+        .state("car_query",{
+            url:"/car_query",
+            templateUrl: "js/view/car/car_query/car_query.html",
+            controller:'car_query_controller'
+        })
+        .state("car_query_details",{
+            url:"/car_query_details/vin/{vin}/id/{id}",
+            templateUrl: "js/view/car/car_query/car_query_details.html",
+            controller:'car_query_details_controller'
+        })
+        .state("instruction_list", {
+            url: "/instruction_list",
+            templateUrl: "js/view/dispatch/instruction_list.html",
+            controller:"instruction_list_controller"
+        })
+        .state("look_instruction_list_details", {
+            url: "/look_instruction_list_details/id/{id}/from/{from}",
+            templateUrl: "js/view/dispatch/look_instruction_list_details.html",
+            controller:"look_instruction_list_details_controller"
+        })
+        .state("_look_instruction_list_details", {
+            url: "/look_instruction_list_details/id/{id}/instruction_id/{instruction_id}/timeStart/{timeStart}/timeEnd/{timeEnd}/from/{from}?refer",
+            templateUrl: "js/view/dispatch/look_instruction_list_details.html",
+            controller:"look_instruction_list_details_controller"
+        })
+        .state("driver_information",{
+            url:"/driver_information",
+            templateUrl: "js/view/truck/driver_information.html",
+            controller:'driver_information_controller'
+        })
+        .state("driver_information_details",{
+            url:"/driver_information_details/driverId/{driverId}",
+            templateUrl: "js/view/truck/driver_information_details.html",
+            controller:'driver_information_details_controller'
+        })
+        .state("car_statistics",{
+            url:"/car_statistics",
+            templateUrl: "js/view/car/car_statistics.html",
+            controller:'car_statistics_controller'
+        })
+
+        // 结算管理
+        .state("settlement_management",{
+            url:"/settlement_management",
+            templateUrl: "js/view/settlement/settlement_management.html",
+            controller:'settlement_management_controller'
+        })
+        // 结算管理详情
+        .state("settlement_management_detail",{
+            url:"/settlement_management_detail/id/{id}",
+            templateUrl: "js/view/settlement/settlement_management_detail.html",
+            controller:'settlement_management_detail_controller'
+        })
+
+        //管理员设置
+        .state("setting_city",{
+            url:"/setting_city",
+            templateUrl: "js/view/system_settings/setting_city.html",
+            controller:'setting_city_controller'
+        })
+        .state("setting_line",{
+            url:"/setting_line",
+            templateUrl: "js/view/system_settings/setting_line.html",
+            controller:'setting_line_controller'
+        })
+        .state("setting_client",{
+            url:"/setting_client",
+            templateUrl: "js/view/system_settings/setting_client/setting_client.html",
+            controller:'setting_client_controller'
+        })
+        .state("add_setting_client",{
+            url:"/add_setting_client",
+            templateUrl: "js/view/system_settings/setting_client/add_setting_client.html",
+            controller:'setting_add_client_controller'
+        })
+        .state("setting_client_details",{
+            url:"/setting_client_details/id/{id}",
+            templateUrl: "js/view/system_settings/setting_client/setting_client_details.html",
+            controller:'setting_client_details_controller'
+        })
+        .state("setting_dealer",{
+            url:"/setting_dealer",
+            templateUrl: "js/view/system_settings/setting_dealer/setting_dealer.html",
+            controller:'setting_dealer_controller'
+        })
+        .state("add_setting_dealer",{
+            url:"/add_setting_dealer",
+            templateUrl: "js/view/system_settings/setting_dealer/add_setting_dealer_details.html",
+            controller:'add_setting_dealer_controller'
+        })
+        .state("setting_dealer_details",{
+            url:"/setting_dealer_details/{dealer_id}",
+            templateUrl: "js/view/system_settings/setting_dealer/setting_dealer_details.html",
+            controller:'setting_dealer_details_controller'
+        })
+        .state("setting_shipments",{
+            url:"/setting_shipments",
+            templateUrl: "js/view/system_settings/setting_shipments/setting_shipments.html",
+            controller:'setting_shipments_controller'
+        })
+        .state("add_setting_shipments",{
+            url:"/add_setting_shipments",
+            templateUrl: "js/view/system_settings/setting_shipments/add_setting_shipments.html",
+            controller:'add_setting_shipments_controller'
+        })
+        .state("setting_shipments_details",{
+            url:"/setting_shipments_details/{shipments_id}",
+            templateUrl: "js/view/system_settings/setting_shipments/setting_shipments_details.html",
+            controller:'setting_shipments_details_controller'
+        })
+
+}]);

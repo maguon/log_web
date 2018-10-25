232db30a31c6b20f2b7f87437d5565b3d1a52ca2
star920626 <jing.zhang@myxxjs.com>
2018-10-25 13:09:04 +0800

    bug

--- web/js/controller/damage/damage_report_controller.js
+++ web/js/controller/damage/damage_report_controller.js
@@ -49,9 +49,10 @@ app.controller("damage_report_controller", ["$scope", "$host", "_basic", functio
 
         // 根据填充完毕的完整VIN信息进行精确查询
         if ($scope.vinCode.length >= 17) {
-            $scope.vinIdCode=$scope.vinCode.slice(-17);
+            $scope.vinIdCode=$scope.vinCode.split('       ')[1];
             $scope.carIdCode=$scope.vinCode.split('       ')[0];
-            $scope.vinCode=$scope.vinCode.slice(-17);
+            $scope.vinCode=$scope.vinCode.split('       ')[1];
+            console.log( $scope.vinIdCode,$scope.carIdCode,$scope.vinCode)
             _basic.get($host.api_url + "/carDamageDeclare?vin=" + $scope.vinIdCode+"&carId="+ $scope.carIdCode).then(function (data) {
                 if (data.success === true && data.result.length !== 0) {
                     $scope.vinCheck = true;

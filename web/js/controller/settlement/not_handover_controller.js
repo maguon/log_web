71b13b213ca894eceaf62257c3c060e56f59a55f
star920626 <jing.zhang@myxxjs.com>
2018-11-05 09:51:28 +0800

    未返还交接单查询功能中先添加时间   下载功能添加

--- web/js/controller/settlement/not_handover_controller.js
+++ web/js/controller/settlement/not_handover_controller.js
@@ -1,7 +1,8 @@
 app.controller("not_handover_controller", ["$scope", "$host", "_basic", function ($scope, $host, _basic) {
     $scope.start = 0;
     $scope.size = 11;
-
+    $("#pre").hide();
+    $("#next").hide();
     // 信息获取
     function getMsg() {
         // 委托方
@@ -53,6 +54,40 @@ app.controller("not_handover_controller", ["$scope", "$host", "_basic", function
             }
         })
     };
+    // 数据导出
+    $scope.export = function () {
+        if ($scope.planTimeStart == undefined || $scope.planTimeEnd == undefined) {
+            swal('请输入完整的查询时间', "", "error");
+        }
+        else {
+            var obj = {
+                carLoadStatus: 2,
+                vinCode: $scope.VIN,
+                entrustId: $scope.client,
+                routeEndId: $scope.arrive_city,
+                receiveId: $scope.receiveId,
+                dpRouteTaskId: $scope.instructionNum,
+                driveId: $scope.driverIdMod,
+                receivedDateStart: $scope.planTimeStart,
+                receivedDateEnd: $scope.planTimeEnd
+
+            };
+            swal({
+                    title: "确定导出未返还交接单表？",
+                    text: "",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    closeOnConfirm: true,
+                    closeOnCancel: true
+                },
+                function () {
+                    window.open($host.api_url + "/notSettleHandover.csv?" + _basic.objToUrl(obj));
+                })
+        }
+    };
 
 
     //查询
@@ -61,40 +96,46 @@ app.controller("not_handover_controller", ["$scope", "$host", "_basic", function
         getNotHandover();
     }
 
-    function  getNotHandover(){
-        _basic.get($host.api_url + "/notSettleHandover?transferFlag=0&" + _basic.objToUrl({
-            carLoadStatus:2,
-            vinCode:$scope.VIN,
-            entrustId:$scope.client,
-            routeEndId:$scope.arrive_city,
-            receiveId:$scope.receiveId,
-            dpRouteTaskId:$scope.instructionNum,
-            driveId:$scope.driverIdMod,
-            receivedDateStart:$scope.planTimeStart,
-            receivedDateEnd:$scope.planTimeEnd,
-            start:$scope.start.toString(),
-            size:$scope.size
-        })).then(function (data) {
-            if (data.success === true) {
-                $scope.boxArray = data.result;
-                $scope.notHandoverArray = $scope.boxArray.slice(0, 10);
-                if ($scope.start > 0) {
-                    $("#pre").show();
+    function  getNotHandover() {
+        if ($scope.planTimeStart == undefined || $scope.planTimeEnd == undefined) {
+            swal('请输入完整的查询时间', "", "error");
+            $scope.notHandoverArray = [];
+        }
+        else {
+            _basic.get($host.api_url + "/notSettleHandover?transferFlag=0&" + _basic.objToUrl({
+                carLoadStatus: 2,
+                vinCode: $scope.VIN,
+                entrustId: $scope.client,
+                routeEndId: $scope.arrive_city,
+                receiveId: $scope.receiveId,
+                dpRouteTaskId: $scope.instructionNum,
+                driveId: $scope.driverIdMod,
+                receivedDateStart: $scope.planTimeStart,
+                receivedDateEnd: $scope.planTimeEnd,
+                start: $scope.start.toString(),
+                size: $scope.size
+            })).then(function (data) {
+                if (data.success === true) {
+                    $scope.boxArray = data.result;
+                    $scope.notHandoverArray = $scope.boxArray.slice(0, 10);
+                    if ($scope.start > 0) {
+                        $("#pre").show();
+                    }
+                    else {
+                        $("#pre").hide();
+                    }
+                    if (data.result.length < $scope.size) {
+                        $("#next").hide();
+                    }
+                    else {
+                        $("#next").show();
+                    }
                 }
                 else {
-                    $("#pre").hide();
+                    swal(data.msg, "", "error");
                 }
-                if (data.result.length < $scope.size) {
-                    $("#next").hide();
-                }
-                else {
-                    $("#next").show();
-                }
-            }
-            else {
-                swal(data.msg, "", "error");
-            }
-        });
+            });
+        }
     }
 
     //打开模态框
@@ -120,6 +161,5 @@ app.controller("not_handover_controller", ["$scope", "$host", "_basic", function
         getNotHandover();
     };
     getMsg();
-    getNotHandover();
 
 }])

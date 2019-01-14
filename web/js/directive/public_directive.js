bb8a03a89f00523befe419651360b9447b2403a5
star920626 <jing.zhang@myxxjs.com>
2019-01-14 16:38:05 +0800

    修改指令计划中的时间

--- web/js/directive/public_directive.js
+++ web/js/directive/public_directive.js
@@ -217,7 +217,28 @@ publicDirective.directive("date", function () {
         }
     }
 });
-
+/*
+ * timepicker  时间选择触发
+ * */
+publicDirective.directive("time", function () {
+    return {
+        restrict: "A",
+        link: function () {
+            $('.timepicker').pickatime({
+                default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+                fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+                twelvehour: false, // Use AM/PM or 24-hour format
+                donetext: '确定', // text for done-button
+                cleartext: '清除', // text for clear-button
+                canceltext: '取消', // Text for cancel-button
+                autoclose: true, // automatic close timepicker
+                ampmclickable: true, // make AM PM clickable
+                aftershow: function () {
+                } //Function for after opening timepicker
+            });
+        }
+    }
+});
 
 publicDirective.directive("dateFilter", ["$filter", function ($filter) {
     var dateFilter = $filter("date");

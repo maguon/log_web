f1b5130c4f62ef78155a7b4a06ab02b3dcaee86c
star920626 <jing.zhang@myxxjs.com>
2019-04-22 11:15:40 +0800

    超油统计修改

--- web/js/controller/statistics/car_refueling_statistics_controller.js
+++ web/js/controller/statistics/car_refueling_statistics_controller.js
@@ -13,58 +13,57 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
     // 货车加油量按月统计
     var truckRefuelingVolCountMonth = [
         {
-            name: '内部加油',
+            name: '按月统计',
             data: [],
             color: '#26C6DA'
-        },
-        {
-            name: '外部加油',
-            data: [],
-            color: '#FF7E7E'
         }
     ];
 
-    // 货车加油金额按月统计
-    var truckRefuelingMoneyCountMonth = [
+    // 货车加尿素量按月统计
+    var truckUreaVolCountMonth = [
         {
-            name: '内部加油',
+            name: '按月统计',
             data: [],
             color: '#26C6DA'
-        },
+        }
+    ];
+
+    // 超量金额按月统计
+    var truckRefuelingMoneyCountMonth = [
         {
-            name: '外部加油',
+            name: '按月统计',
             data: [],
-            color: '#FF7E7E'
+            color: '#26C6DA'
         }
     ];
 
     // 货车加油量按周统计
     var truckRefuelingVolCountWeek = [
         {
-            name: '内部加油',
+            name: '按周统计',
             data: [],
             color: '#26C6DA'
-        },
+        }
+    ];
+
+    // 货车加尿素量按周统计
+    var truckUreaVolCountWeek = [
         {
-            name: '外部加油',
+            name: '按周统计',
             data: [],
-            color: '#FF7E7E'
+            color: '#26C6DA'
         }
     ];
 
     // 货车加油金额按周统计
     var truckRefuelingMoneyCountWeek = [
         {
-            name: '内部加油',
+            name: '按周统计',
             data: [],
             color: '#26C6DA'
-        },
-        {
-            name: '外部加油',
-            data: [],
-            color: '#FF7E7E'
         }
     ];
+
 
     // 点击搜索按钮根据日期搜索货车加油信息
     $scope.searchTruckRefuelingCount = function () {
@@ -81,36 +80,68 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
         if (end === undefined) {
             end = $scope.endInitial
         }
-        _basic.get($host.api_url + "/refuelMonthStat?monthStart=" + start + "&monthEnd=" + end).then(function (data) {
+        _basic.get($host.api_url + "/driveOilMonthStat?settleStatus=2&monthStart=" + start + "&monthEnd=" + end).then(function (data) {
             if (data.success === true) {
                 data.result.reverse();
                 // 货车加油X轴月份
                 $scope.refuelingVolMonth = [];
-                $scope.refuelingMoneyMonth = [];
                 // 初始化统计数
                 truckRefuelingVolCountMonth[0].data = [];
-                truckRefuelingVolCountMonth[1].data = [];
                 truckRefuelingMoneyCountMonth[0].data = [];
-                truckRefuelingMoneyCountMonth[1].data = [];
                 if(data.result.length > 0){
                     for (var i = 0; i < data.result.length; i++) {
-                        if(data.result[i].refuel_address_type === 1){
-                            truckRefuelingVolCountMonth[0].data.push(Math.ceil(data.result[i].total_vol));
-                            truckRefuelingMoneyCountMonth[0].data.push(Math.ceil(data.result[i].total_money));
+                            truckRefuelingVolCountMonth[0].data.push(Math.ceil(data.result[i].actual_oil));
                             if($scope.refuelingVolMonth.indexOf(data.result[i].y_month) === -1){
                                 $scope.refuelingVolMonth.push(data.result[i].y_month);
                             }
                         }
-                        else{
-                            truckRefuelingVolCountMonth[1].data.push(Math.ceil(data.result[i].total_vol));
-                            truckRefuelingMoneyCountMonth[1].data.push(Math.ceil(data.result[i].total_money));
-                            if($scope.refuelingMoneyMonth.indexOf(data.result[i].y_month) === -1){
-                                $scope.refuelingMoneyMonth.push(data.result[i].y_month);
-                            }
+                    }
+                $scope.showTruckRefueling_month();
+            }
+            else {
+                swal(data.msg, "", "error");
+            }
+        });
+
+
+        _basic.get($host.api_url + "/driveUreaMonthStat?settleStatus=2&monthStart=" + start + "&monthEnd=" + end).then(function (data) {
+            if (data.success === true) {
+                data.result.reverse();
+                // 货车加油X轴月份
+                $scope.ureaVolMonth = [];
+                // 初始化统计数
+                truckUreaVolCountMonth[0].data = [];
+                if(data.result.length > 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        truckUreaVolCountMonth[0].data.push(Math.ceil(data.result[i].actual_urea));
+                        if($scope.ureaVolMonth.indexOf(data.result[i].y_month) === -1){
+                            $scope.ureaVolMonth.push(data.result[i].y_month);
                         }
                     }
                 }
-                $scope.showTruckRefueling_month();
+                $scope.showTruckUrea_month();
+            }
+            else {
+                swal(data.msg, "", "error");
+            }
+        });
+
+
+        _basic.get($host.api_url + "/driveOilMoneyMonthStat?settleStatus=2&monthStart=" + start + "&monthEnd=" + end).then(function (data) {
+            if (data.success === true) {
+                data.result.reverse();
+                // 货车加油X轴月份
+                $scope.refuelingMoneyMonth = [];
+                // 初始化统计数
+                truckRefuelingMoneyCountMonth[0].data = [];
+                if(data.result.length > 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        truckRefuelingMoneyCountMonth[0].data.push(Math.ceil(data.result[i].actual_money));
+                        if($scope.refuelingMoneyMonth.indexOf(data.result[i].y_month) === -1){
+                            $scope.refuelingMoneyMonth.push(data.result[i].y_month);
+                        }
+                    }
+                }
                 $scope.showTruckRefuelingMoney_month();
             }
             else {
@@ -119,38 +150,74 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
         });
     };
 
+
     // 获取货车加油按周统计信息
     $scope.getTruckRefuelingWeekInfo = function () {
-        _basic.get($host.api_url + "/refuelWeekStat?start=0&size=10").then(function (data) {
+        _basic.get($host.api_url + "/driveOilWeekStat?settleStatus=2&start=0&size=10").then(function (data) {
             if (data.success === true) {
                 data.result.reverse();
                 // 货车加油X轴周数
                 $scope.refuelingVolWeek = [];
-                $scope.refuelingMoneyWeek = [];
+
                 // 初始化统计数
                 truckRefuelingVolCountWeek[0].data = [];
-                truckRefuelingVolCountWeek[1].data = [];
-                truckRefuelingMoneyCountWeek[0].data = [];
-                truckRefuelingMoneyCountWeek[1].data = [];
+
+
                 if(data.result.length > 0){
                     for (var i = 0; i < data.result.length; i++) {
-                        if(data.result[i].refuel_address_type === 1){
-                            truckRefuelingVolCountWeek[0].data.push(Math.ceil(data.result[i].total_vol));
-                            truckRefuelingMoneyCountWeek[0].data.push(Math.ceil(data.result[i].total_money));
+                            truckRefuelingVolCountWeek[0].data.push(Math.ceil(data.result[i].actual_oil));
                             if($scope.refuelingVolWeek.indexOf(data.result[i].y_week) === -1){
                                 $scope.refuelingVolWeek.push(data.result[i].y_week);
                             }
-                        }
-                        else{
-                            truckRefuelingVolCountWeek[1].data.push(Math.ceil(data.result[i].total_vol));
-                            truckRefuelingMoneyCountWeek[1].data.push(Math.ceil(data.result[i].total_money));
-                            if($scope.refuelingMoneyWeek.indexOf(data.result[i].y_week) === -1){
-                                $scope.refuelingMoneyWeek.push(data.result[i].y_week);
-                            }
-                        }
                     }
                 }
                 $scope.showTruckRefueling_week();
+            }
+            else {
+                swal(data.msg, "", "error");
+            }
+        });
+        _basic.get($host.api_url + "/driveUreaWeekStat?settleStatus=2&start=0&size=10").then(function (data) {
+            if (data.success === true) {
+                data.result.reverse();
+                // 货车加油X轴周数
+
+                $scope.ureaVolWeek = [];
+
+                // 初始化统计数
+
+                truckUreaVolCountWeek[0].data = [];
+
+                if(data.result.length > 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        truckUreaVolCountWeek[0].data.push(Math.ceil(data.result[i].actual_urea));
+                        if($scope.ureaVolWeek.indexOf(data.result[i].y_week) === -1){
+                            $scope.ureaVolWeek.push(data.result[i].y_week);
+                        }
+                    }
+                }
+                $scope. showTruckUrea_week();
+            }
+            else {
+                swal(data.msg, "", "error");
+            }
+        });
+        _basic.get($host.api_url + "/driveOilMoneyWeekStat?settleStatus=2&start=0&size=10").then(function (data) {
+            if (data.success === true) {
+                data.result.reverse();
+                // 货车加油X轴周数
+                $scope.refuelingMoneyWeek = [];
+                // 初始化统计数
+                truckRefuelingMoneyCountWeek[0].data = [];
+
+                if(data.result.length > 0){
+                    for (var i = 0; i < data.result.length; i++) {
+                        truckRefuelingMoneyCountWeek[0].data.push(Math.ceil(data.result[i].actual_money));
+                        if($scope.refuelingMoneyWeek.indexOf(data.result[i].y_week) === -1){
+                            $scope.refuelingMoneyWeek.push(data.result[i].y_week);
+                        }
+                    }
+                }
                 $scope.showTruckRefuelingMoney_week();
             }
             else {
@@ -158,6 +225,7 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
             }
         });
     };
+
 
     // 显示货车加油量按月统计折线图
     $scope.showTruckRefueling_month = function () {
@@ -205,11 +273,58 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
         });
     };
 
+    // 显示货车加尿素按月统计折线图
+    $scope.showTruckUrea_month =function (){
+        $("#truckUreaMonth").highcharts({
+            title: {
+                text: '加尿素统计',
+                align: "left",
+                style: {
+                    color: '#616161',
+                    fontWeight: 'bold'
+                },
+                x: 31
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                categories: $scope.ureaVolMonth,
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ''
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}：</td>' +
+                '<td style="padding:0"><b>{point.y:.1f} 升</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: truckUreaVolCountMonth
+        });
+    }
+
+
     // 显示货车加油金额按月统计折线图
     $scope.showTruckRefuelingMoney_month = function () {
         $("#truckRefuelingMoneyMonth").highcharts({
             title: {
-                text: '加油金额统计',
+                text: '超量金额统计',
                 align: "left",
                 style: {
                     color: '#616161',
@@ -299,6 +414,56 @@ app.controller("car_refueling_statistics_controller", ["$scope", "$host", "_basi
             series: truckRefuelingVolCountWeek
         });
     };
+
+    // 显示货车加油量按周统计折线图
+    $scope.showTruckUrea_week = function () {
+        $("#truckUreaWeek").highcharts({
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: '加尿素统计',
+                align: "left",
+                style: {
+                    color: '#616161',
+                    fontWeight: 'bold'
+                },
+                x: 31
+            },
+            subtitle: {
+                text: ''
+            },
+            xAxis: {
+                categories: $scope.ureaVolWeek,
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: ''
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}：</td>' +
+                '<td style="padding:0"><b>{point.y:.1f} 升</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: truckUreaVolCountWeek
+        });
+    };
+
 
     // 显示货车加油金额按周统计折线图
     $scope.showTruckRefuelingMoney_week = function () {

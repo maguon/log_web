03dcb4f02866fbd2cfd879cd8d441eb43323a27a
star920626 <jing.zhang@myxxjs.com>
2019-06-12 14:26:12 +0800

    违章扣款导入

--- web/js/controller/truck/peccancy_withhold_controller.js
+++ web/js/controller/truck/peccancy_withhold_controller.js
@@ -4,7 +4,219 @@
 app.controller("peccancy_withhold_controller", ["$scope", "$state", "_basic", "_config", "$host", function ($scope, $state, _basic, _config, $host) {
     $scope.start = 0;
     $scope.size = 11;
+    var userType = _basic.getSession(_basic.USER_TYPE);
     var userId = _basic.getSession(_basic.USER_ID);
+
+
+    $scope.num = 0;
+    $scope.flag=false;
+    $scope.tableBox = true;
+    $scope.success_data_box = false;
+    $scope.dataBox = false;
+    $scope.local_isSuccesss = false;
+    $scope.upload_isSuccesss = false;
+    $scope.show_error = false;
+    $scope.error_msg = false;
+    $scope.rightNumber = 0;
+    $scope.errorNumber = 0;
+    $scope.tableHeader = [];
+    $scope.fileType = "";
+    // 表头原始数据
+    $scope.tableHeadeArray = [];
+    // 主体原始错误数据
+    $scope.tableContentErrorFilter = [];
+    // 主体原始成功数据
+    $scope.tableContentFilter = [];
+    $scope.ImportedFilesList = [];
+    // 跳转
+    $scope.importFile = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.importFile ').addClass("active");
+        $("#importFile").addClass("active");
+        $("#importFile").show();
+    };
+    $scope.lookMyselfFile = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.lookMyselfFile ').addClass("active");
+        $("#lookMyselfFile").addClass("active");
+        $("#lookMyselfFile").show();
+    };
+    $scope.lookMyselfFile();
+
+    $("#pre").hide();
+    $("#next").hide();
+
+    // 过滤条件数据
+    var colObjs = [
+        {name: '司机', type: 'string',require: true},
+        {name: '货车牌号', type: 'string',require: true},
+        {name: '扣分', type: 'number', require: true},
+        {name: '买分金额', type: 'number', require: true},
+        {name: '交通罚款', type: 'number', require: true},
+        {name: '处理金额', type: 'number', require: true},
+        {name: '个人承担金额', type: 'number', require: true},
+        {name: '公司承担金额', type: 'number', require: true},
+        {name: '违章时间', type: 'string', require: true},
+        {name: '处理时间', type: 'string', require: true},
+        {name: '城市ID', type: 'number', require: true},
+        {name: '违章地点', type: 'string', require: true},
+        {name: '备注', type: 'string', require: false}];
+    // 头部条件判断
+    $scope.titleFilter = function (headerArray) {
+        if (colObjs.length != headerArray.length) {
+            return false;
+        } else {
+            for (var i in headerArray) {
+                if (colObjs[i].name != headerArray[i]) {
+                    return false
+                }
+            }
+        }
+    };
+    // 主体条件判断
+    $scope.ContentFilter = function (contentArray) {
+
+        for (var i = 0; i < contentArray.length; i++) {
+            var flag = true;
+            var isNumber;
+            for (var j = 0; j < colObjs.length; j++) {
+                if (colObjs[j].require) {
+                    if (contentArray[i][j] == null && contentArray[i][j].length == 0) {
+                        $scope.errorNumber = $scope.errorNumber + 1;
+                        $scope.tableContentErrorFilter.push(contentArray[i]);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (contentArray[i][j] == '' || isNaN(contentArray[i][j])) {
+                    isNumber = "string"
+                } else {
+                    isNumber = "number"
+                }
+                if (colObjs[j].type != isNumber && contentArray[i][j] != '' &&colObjs[j].require ) {
+                    $scope.errorNumber = $scope.errorNumber + 1;
+                    $scope.tableContentErrorFilter.push(contentArray[i]);
+                    flag = false;
+                    break;
+                }
+                if (colObjs[j].type=='string'&&(colObjs[j].length && colObjs[j].length != contentArray[i][j].length)) {
+                    $scope.errorNumber = $scope.errorNumber + 1;
+                    $scope.tableContentErrorFilter.push(contentArray[i]);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true) {
+                $scope.rightNumber = $scope.rightNumber + 1;
+                $scope.tableContentFilter.push(contentArray[i]);
+            }
+        }
+    };
+
+    $scope.fileUpload = function () {
+        $("#buttonImport").attr("disabled",true);
+        _basic.formPost($("#file_upload_form"), $host.api_url + '/user/' + userId + '/drivePeccancyFile' , function (data) {
+            if (data.success == true) {
+                $scope.$apply(function () {
+                    $scope.upload_error_array_num =data.result.failedCase;
+                    $scope.orginData_Length=data.result.failedCase+data.result.successedInsert;
+                    $scope.num=data.result.successedInsert;
+                    $scope.local_isSuccesss = false;
+                    $scope.upload_isSuccesss = true;
+                    $("#file_upload_form").disabled=true;
+                    $("#buttonImport").attr("disabled",false);
+                    swal('正确:'+$scope.num+'错误:'+$scope.upload_error_array_num,"", "success")
+                });
+
+            }
+            else {
+                $("#buttonImport").attr("disabled",false);
+                swal(data.msg, "", "error");
+            }
+        });
+    };
+
+    // 展示上传的错误数据
+    $scope.show_error_msg = function () {
+        $scope.error_msg = !$scope.error_msg;
+    };
+
+    $scope.fileChange = function (file) {
+        // 表头原始数据
+        $scope.tableHeadeArray = [];
+        // 主体原始错误数据
+        $scope.tableContentErrorFilter = [];
+        // 主体原始成功数据
+        $scope.tableContentFilter = [];
+        $scope.rightNumber = 0;
+        $scope.errorNumber = 0;
+        $(file).parse({
+            config: {
+                complete: function (result) {
+                    $scope.$apply(function () {
+                        if(result==null ||result.data==null ||result.data.length ==0){
+                            swal("文件类型错误");
+                        } else {
+                            $scope.tableHeadeArray = result.data[0];
+                            $scope.tableBox = false;
+                            // 表头校验
+                            if ($scope.titleFilter($scope.tableHeadeArray) != false) {
+                                // 主体内容校验
+                                var content_filter_array = result.data.slice(1, result.data.length);
+                                var con_line = [];
+                                // excel换行过滤
+                                for (var i = 0; i < content_filter_array.length; i++) {
+                                    if (content_filter_array[i].length == 1 && content_filter_array[i][0] == "") {
+                                        break;
+                                    } else {
+                                        con_line.push(content_filter_array[i]);
+                                    }
+                                }
+                                $scope.ContentFilter(con_line);
+                                if ($scope.tableContentErrorFilter.length == 0) {
+                                    $scope.success_data_box = true;
+                                    $scope.dataBox = false;
+                                    swal("正确条数" + $scope.tableContentFilter.length);
+                                    // 总条数
+                                    $scope.orginData_Length = $scope.tableContentFilter.length;
+                                    $scope.local_isSuccesss = true;
+                                } else {
+                                    $scope.success_data_box = false;
+                                    $scope.dataBox = true;
+                                    swal("错误条数" + $scope.tableContentErrorFilter.length);
+                                }
+                                $scope.tableHeader = result.data[0];
+                            }
+                            else {
+                                swal("表头格式错误", "", "error");
+                                $scope.tableBox = true;
+                            }
+
+                        }
+
+                    });
+
+                }
+            },
+            before: function (file, inputElem) {
+                $scope.fileType = file.type;
+            },
+            error: function (err, file, inputElem, reason) {
+                console.log(err)
+            },
+            complete: function (val) {
+            }
+        })
+    };
+
+
+
+
+
     //司机
     function getDriveNameList () {
         _basic.get($host.api_url + "/drive").then(function (data) {
@@ -180,7 +392,6 @@ app.controller("peccancy_withhold_controller", ["$scope", "$state", "_basic", "_
     function getPeccancyData(){
         _basic.get($host.api_url + "/drivePeccancy?" + _basic.objToUrl({
             driveId:$scope.driverId,
-            statStatus:$scope.peccancyStu,
             truckType:$scope.truckType,
             startDateStart:$scope.getStartTime,
             startDateEnd:$scope.getEndTime,
@@ -218,7 +429,6 @@ app.controller("peccancy_withhold_controller", ["$scope", "$state", "_basic", "_
     $scope.export = function () {
         var obj = {
             driveId:$scope.driverId,
-            fineStatus:$scope.peccancyStu,
             truckType:$scope.truckType,
             startDateStart:$scope.getStartTime,
             startDateEnd:$scope.getEndTime,

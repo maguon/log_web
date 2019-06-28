1d1371537ae1cc356d114706125ed1db0260af79
star920626 <jing.zhang@myxxjs.com>
2019-06-28 16:52:39 +0800

    未交接到已交接

--- web/js/controller/settlement/not_handover_controller.js
+++ web/js/controller/settlement/not_handover_controller.js
@@ -1,8 +1,13 @@
 app.controller("not_handover_controller", ["$scope", "$host", "_basic", function ($scope, $host, _basic) {
     $scope.start = 0;
     $scope.size = 11;
+    var userId = _basic.getSession(_basic.USER_ID);
+    $scope.ArrayList=[];
+    $scope.carIds=[];
     $("#pre").hide();
     $("#next").hide();
+
+
     // 信息获取
     function getMsg() {
         // 委托方
@@ -214,6 +219,106 @@ app.controller("not_handover_controller", ["$scope", "$host", "_basic", function
         $('#settlementItem').modal('open');
     }
 
+
+
+    //点击加号 生成数组
+    $scope.getArr = function (el){
+        if($scope.ArrayList.length!==0){
+            if(
+                $scope.ArrayList[0].make_name==el.make_name&&
+                $scope.ArrayList[0].e_short_name==el.e_short_name&&
+                $scope.ArrayList[0].route_end==el.route_end&&
+                $scope.ArrayList[0].route_start==el.route_start&&
+                $scope.ArrayList[0].addr_name==el.addr_name&&
+                $scope.ArrayList[0].r_short_name==el.r_short_name
+            ){
+                for (var i = 0; i < $scope.ArrayList.length; i++) {
+                    if ($scope.ArrayList[i].vin === el.vin) {
+                        swal('不能重复添加相同车辆!',"", "error");
+                        return;
+                    }
+                }
+                $scope.ArrayList.push(el);
+            }
+            else{
+                swal('添加失败',"", "error");
+            }
+        }
+        else{
+            $scope.ArrayList.push(el);
+        }
+    }
+    //删除
+    $scope.deleteSingle =function (_obj,_arr){
+        var length = _arr.length;
+        for (var i = 0; i < length; i++) {
+            if (_arr[i] === _obj) {
+                if (i === 0) {
+                    _arr.shift(); //删除并返回数组的第一个元素
+                    return _arr;
+                }
+                else if (i === length - 1) {
+                    _arr.pop();  //删除并返回数组的最后一个元素
+                    return _arr;
+                }
+                else {
+                    _arr.splice(i, 1); //删除下标为i的元素
+                    return _arr;
+                }
+            }
+        }
+        $scope.ArrayList =_arr;
+    }
+
+    //未交接到已交接
+    $scope.addArr = function (){
+        for (var i = 0; i < $scope.ArrayList.length; i++) {
+            $scope.carIds.push($scope.ArrayList[i].car_id)
+        }
+        $('#addSettlementItem').modal('open');
+    }
+
+    $scope.addSettlementItem = function (){
+        swal({
+            title: "确定交接左侧所有车辆吗？",
+            text: "",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "确定",
+            cancelButtonText: "取消"
+        }).then(function (result) {
+            if (result.value) {
+                if($scope.addNumberId!== undefined&&$scope.addHandoverReceiveStartTime!== undefined){
+                    _basic.post($host.api_url + "/user/" + userId + "/settleHandoverAll", {
+                        "serialNumber":  $scope.addNumberId,
+                        "entrustId":   $scope.ArrayList[0].entrust_id,
+                        "receivedDate": $scope.addHandoverReceiveStartTime,
+                        "routeStartId":  $scope.ArrayList[0].route_start_id,
+                        "routeEndId":  $scope.ArrayList[0].route_end_id,
+                        "receiveId":  $scope.ArrayList[0].receive_id,
+                        "carCount":$scope.ArrayList.length,
+                        "remark":$scope.newRemark,
+                        "carIds":$scope.carIds
+                    }).then(function (data) {
+                        if (data.success == true) {
+                            $scope.ArrayList=[];
+                            $('#addSettlementItem').modal('close');
+                            getNotHandover();
+                            swal("交接成功", "", "success");
+                        }
+                        else {
+                            swal(data.msg, "", "error");
+                        }
+                    })
+                }
+              else {
+                    swal("请填写完整信息！", "", "warning");
+                }
+
+            }
+        })
+    }
 
 
     // 分页

173edf00a1d3d6812e22e230b2d415ab762b4eb5
star920626 <jing.zhang@myxxjs.com>
2019-09-25 13:22:31 +0800

    质检统计添加到仓储管理员

--- web/js/router/storage_manager_router.js
+++ web/js/router/storage_manager_router.js
@@ -199,10 +199,10 @@ app.config(['$stateProvider',"$urlRouterProvider",function($stateProvider,$urlRo
             templateUrl: "js/view/statistics/transport_planning_statistics.html",
             controller:"transport_planning_statistics_controller"
         })
-        .state("vehicle_damage_statistics", {
-            url:"/vehicle_damage_statistics",
-            templateUrl: "js/view/statistics/vehicle_damage_statistics.html",
-            controller:"vehicle_damage_statistics_controller"
+        .state("car_detection_statistics", {
+            url:"/car_detection_statistics",
+            templateUrl: "js/view/damage/car_detection_statistics.html",
+            controller:"car_detection_statistics_controller"
         })
         // 质损申报
         .state("damage_declaration",{

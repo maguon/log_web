01ab87b0c10dfb7d60ce0808ab5f0b9db8eee5aa
yjyvsdream <jingyuan.yu@myxxjs.com>
2020-08-13 10:51:19 +0800

    新加机能：检车任务 数据

--- web/js/controller/damage/car_detection_statistics_controller.js
+++ web/js/controller/damage/car_detection_statistics_controller.js
@@ -1,5 +1,9 @@
-
-app.controller("car_detection_statistics_controller", ["$scope", "$host", "_basic", function ($scope, $host, _basic) {
+app.controller("car_detection_statistics_controller", ["$scope", "$rootScope", "$host", "_basic", function ($scope, $rootScope, $host, _basic) {
+    // 用户ID
+    var userId = _basic.getSession(_basic.USER_ID);
+    // 翻页用
+    $scope.start = 0;
+    $scope.size = 21;
 
     //管理员
     function getManagerList() {
@@ -13,11 +17,12 @@ app.controller("car_detection_statistics_controller", ["$scope", "$host", "_basi
                 });
             }
         });
-    };
+    }
+
     //查询功能
     $scope.searchMoveCarList= function (){
         getMoveCar();
-    }
+    };
 
     function getMoveCar(){
         var obj ={
@@ -40,8 +45,8 @@ app.controller("car_detection_statistics_controller", ["$scope", "$host", "_basi
                 }
             });
         }
-
     }
+
     /*导出*/
     $scope.export = function(){
         var obj ={
@@ -56,10 +61,149 @@ app.controller("car_detection_statistics_controller", ["$scope", "$host", "_basi
         else {
             window.open($host.record_url + "/opRecord.csv?"+_basic.objToUrl(obj));
         }
+    };
+
+    /*** 2020-08-12 追加代码 开始位置 ***/
+
+    // 质检统计
+    $scope.showDetection = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.detection').addClass("active");
+        $("#detection").addClass("active");
+        $("#detection").show();
+    };
+
+    // 检车任务
+    $scope.showDamageQaTask = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.damage').addClass("active");
+        $("#damage").addClass("active");
+        $("#damage").show();
+    };
+
+    /**
+     * 根据条件 查询数据【检车任务】
+     */
+    $scope.getDamageQaTaskList = function () {
+        // 基本检索URL
+        var url = $host.api_url + "/user/" + userId + "/damageQaTask?start=" + $scope.start + "&size=" + $scope.size;
+        // 检索条件
+        var conditionsObj = makeConditions();
+        var conditions = _basic.objNewTo2Url(conditionsObj);
+        // 检索URL
+        url = conditions.length > 0 ? url + "&" + conditions : url;
+        _basic.get(url).then(function (data) {
+            if (data.success) {
+                // 将当前画面的条件
+                $scope.damageQaTaskArray = data.result;
+                $scope.damageQaTaskList = $scope.damageQaTaskArray.slice(0, 20);
+
+                if ($scope.start > 0) {
+                    $("#pre").show();
+                } else {
+                    $("#pre").hide();
+                }
+                if (data.result.length < $scope.size) {
+                    $("#next").hide();
+                } else {
+                    $("#next").show();
+                }
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    };
+
+    // 前一页
+    $scope.preBtn = function () {
+        $scope.start = $scope.start - ($scope.size - 1);
+        $scope.getDamageQaTaskList();
+    };
+
+    // 后一页
+    $scope.nextBtn = function () {
+        $scope.start = $scope.start + ($scope.size - 1);
+        $scope.getDamageQaTaskList();
+    };
+
+    // 组装检索条件。
+    function makeConditions() {
+        var damageDateStart;
+        if ($scope.damageDateStart == undefined || $scope.damageDateStart == '' || $scope.damageDateStart == null) {
+            damageDateStart = null;
+        } else {
+            damageDateStart = moment($scope.damageDateStart).format('YYYYMMDD');
+        }
+
+        var damageDateEnd;
+        if ($scope.damageDateEnd == undefined || $scope.damageDateEnd == '' || $scope.damageDateEnd == null) {
+            damageDateEnd = null;
+        } else {
+            damageDateEnd = moment($scope.damageDateEnd).format('YYYYMMDD');
+        }
+
+        return {
+            dateIdStart: damageDateStart,
+            dateIdEnd: damageDateEnd
+        };
     }
 
+    /**
+     * 根据任务号 查询数据【检车任务详情】
+     */
+    function getDamageQaTaskCarRel(qtId) {
+        // 基本检索URL
+        var url = $host.api_url + "/user/" + userId + "/damageQaTaskCarRel?qtId=" + qtId;
+        _basic.get(url).then(function (data) {
+            if (data.success) {
+                $scope.damageQaTaskCarRelArray = data.result;
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
 
-    getManagerList();
+    /**
+     * 根据任务号 查询数据【检车任务详情】-【用户统计】
+     */
+    function getDamageUserStat(qtId) {
+        // 基本检索URL
+        var url = $host.api_url + "/user/" + userId + "/qtId/" + qtId + "/userStat";
+        _basic.get(url).then(function (data) {
+            if (data.success) {
+                $scope.userStatArray = data.result;
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
 
+    // 【检车任务】打开 模态 检车任务详情
+    $scope.openDamageModal = function (qtId) {
+        // 取得任务号 检车任务详情
+        getDamageQaTaskCarRel(qtId);
+        // 统计用户检车数
+        getDamageUserStat(qtId);
 
+        // 显示模态
+        $('#damageModal').modal('open');
+    };
+
+    /**
+     * 画面初期显示时，用来获取画面必要信息的初期方法。
+     */
+    function initData() {
+        // 默认显示 【检车任务】
+        $scope.showDamageQaTask();
+        // 查询数据【检车任务】
+        $scope.getDamageQaTaskList();
+
+        // 取得 【质检统计】中用的 管理员 列表（修改前代码）
+        getManagerList();
+    }
+    initData();
 }]);

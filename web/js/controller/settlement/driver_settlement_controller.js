96040bde58d23c6fc7c8dd22b574f59de19261a3
star920626 <jing.zhang@myxxjs.com>
2021-01-26 17:04:24 +0800

    司机结算修改

--- web/js/controller/settlement/driver_settlement_controller.js
+++ web/js/controller/settlement/driver_settlement_controller.js
@@ -1,8 +1,34 @@
 app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$scope", function (_basic, _config, $host, $scope) {
     $scope.start = 0;
     $scope.size = 11;
+
     $("#pre").hide();
     $("#next").hide();
+    $("#preSalary").hide();
+    $("#nextSalary").hide();
+    // 跳转
+    $scope.driverOutput = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.driverOutput ').addClass("active");
+        $("#driverOutput").addClass("active");
+        $("#driverOutput").show();
+    };
+    $scope.driverSalary = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.driverSalary ').addClass("active");
+        $("#driverSalary").addClass("active");
+        $("#driverSalary").show();
+    };
+    $scope.driverOutput();
+
+    $("#pre").hide();
+    $("#next").hide();
+
+
     //司机 公司
     function getDriveNameList () {
         _basic.get($host.api_url + "/drive?driveName=").then(function (data) {
@@ -12,19 +38,23 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
                     placeholder: '司机',
                     containerCssClass : 'select2_dropdown'
                 });
+                $('#driveNameSalary').select2({
+                    placeholder: '司机',
+                    containerCssClass : 'select2_dropdown'
+                });
             }
             else {
                 swal(data.msg, "", "error");
             }
         });
-        _basic.get($host.api_url + "/company").then(function (data) {
+       /* _basic.get($host.api_url + "/company").then(function (data) {
             if (data.success === true) {
                 $scope.companyList = data.result;
             }
             else {
                 swal(data.msg, "", "error");
             }
-        });
+        });*/
     }
 
     //获取货车牌号
@@ -33,6 +63,11 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
             if (data.success === true) {
                 $scope.truckNumListAllList = data.result;
                 $('#truckNum').select2({
+                    placeholder: '货车牌号',
+                    containerCssClass: 'select2_dropdown',
+                    allowClear: true
+                });
+                $('#truckNumSalary').select2({
                     placeholder: '货车牌号',
                     containerCssClass: 'select2_dropdown',
                     allowClear: true
@@ -49,6 +84,17 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
         _basic.get($host.api_url + "/company?operateType=" + $scope.operateType).then(function (companyData) {
             if (companyData.success === true) {
                 $scope.companyList = companyData.result;
+            }
+            else {
+                swal(companyData.msg, "", "error");
+            }
+        });
+    };
+    // 根据select选择的所属类型获取所属公司信息
+    $scope.chooseOperateTypeSalary = function () {
+        _basic.get($host.api_url + "/company?operateType=" + $scope.operateTypeSalary).then(function (companyData) {
+            if (companyData.success === true) {
+                $scope.companyListSalary = companyData.result;
             }
             else {
                 swal(companyData.msg, "", "error");
@@ -72,7 +118,7 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
 
             };
             swal({
-                    title: "确定导出司机结算表？",
+                    title: "确定导出司机产值结算表？",
                     text: "",
                     type: "warning",
                     showCancelButton: true,
@@ -82,12 +128,11 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
             }).then(
                 function (result) {
                     if (result.value) {
-                        window.open($host.api_url + "/driveSettle.csv?" + _basic.objToUrl(obj));
+                        window.open($host.api_url + "/driveSettleOutput.csv?" + _basic.objToUrl(obj));
                     }
                 })
         }
     }
-
 
     // 数据导出
     $scope.exportDatail = function () {
@@ -105,7 +150,7 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
 
             };
             swal({
-                title: "确定导出司机结算表详情？",
+                title: "确定导出司机结算产值表详情？",
                 text: "",
                 type: "warning",
                 showCancelButton: true,
@@ -120,7 +165,37 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
                 })
         }
     }
+    $scope.exportSalary =function (){
+        if($scope.instruct_starTimeSalary==undefined||$scope.instruct_endTimeSalary==undefined){
+            swal('请输入完整的查询时间', "", "error");
+            $scope.settlementListSalary=[];
+        }
+        else {
+            var obj = {
+                taskPlanDateStart:  moment($scope.instruct_starTimeSalary).format('YYYYMMDD'),
+                taskPlanDateEnd:  moment($scope.instruct_endTimeSalary).format('YYYYMMDD'),
+                driveId: $scope.drivderIdSalary,
+                companyId: $scope.searchCompanySalary,
+                operateType: $scope.operateTypeSalary,
+                truckId: $scope.truckNumSalary
 
+            };
+            swal({
+                title: "确定导出司机工资表？",
+                text: "",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+                cancelButtonText: "取消"
+            }).then(
+                function (result) {
+                    if (result.value) {
+                        window.open($host.api_url + "/driveSettleSalary.csv?" + _basic.objToUrl(obj));
+                    }
+                })
+        }
+    }
 
     //查询功能
     $scope.getSettlement = function (){
@@ -135,7 +210,7 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
             $scope.settlementList=[];
         }
         else{
-            _basic.get($host.api_url + "/driveSettle?" + _basic.objToUrl({
+            _basic.get($host.api_url + "/driveSettleOutput?" + _basic.objToUrl({
                 taskPlanDateStart:  moment($scope.instruct_starTime).format('YYYYMMDD'),
                 taskPlanDateEnd:  moment($scope.instruct_endTime).format('YYYYMMDD'),
                 driveId: $scope.drivderId,
@@ -167,6 +242,53 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
             });
         }
     }
+
+    //司机工资查询按钮
+    $scope.getSettlementSalary= function (){
+        $scope.start = 0;
+        getSettlementDataSalary();
+    }
+
+    //获取查询数据
+    function getSettlementDataSalary(){
+        if($scope.instruct_starTimeSalary==undefined||$scope.instruct_endTimeSalary==undefined){
+            swal('请输入完整的查询时间', "", "error");
+            $scope.settlementListSalary=[];
+        }
+        else{
+            _basic.get($host.api_url + "/driveSettleSalary?" + _basic.objToUrl({
+                taskPlanDateStart:  moment($scope.instruct_starTimeSalary).format('YYYYMMDD'),
+                taskPlanDateEnd:  moment($scope.instruct_endTimeSalary).format('YYYYMMDD'),
+                driveId: $scope.drivderIdSalary,
+                companyId: $scope.searchCompanySalary,
+                operateType: $scope.operateTypeSalary,
+                truckId: $scope.truckNumSalary,
+                start: $scope.start.toString(),
+                size: $scope.size
+            })).then(function (data) {
+                if (data.success === true) {
+                    $scope.boxArray = data.result;
+                    $scope.settlementListSalary = $scope.boxArray.slice(0, 10);
+                    if ($scope.start > 0) {
+                        $("#preSalary").show();
+                    }
+                    else {
+                        $("#preSalary").hide();
+                    }
+                    if (data.result.length < $scope.size) {
+                        $("#nextSalary").hide();
+                    }
+                    else {
+                        $("#nextSalary").show();
+                    }
+                }
+                else {
+                    swal(data.msg, "", "error");
+                }
+            });
+        }
+    }
+
     // 分页
     $scope.pre_btn = function () {
         $scope.start = $scope.start - ($scope.size-1);
@@ -177,6 +299,20 @@ app.controller("driver_settlement_controller", ["_basic", "_config", "$host", "$
         $scope.start = $scope.start + ($scope.size-1);
         getSettlementData();
     };
+
+
+    // 分页
+    $scope.getPrePageSalary = function () {
+        $scope.start = $scope.start - ($scope.size-1);
+        getSettlementDataSalary();
+    };
+    $scope.getNextPageSalary = function () {
+        $scope.start = $scope.start + ($scope.size-1);
+        getSettlementDataSalary();
+    };
+
+
+
     getTruckNum();
     getDriveNameList ();
 }])
